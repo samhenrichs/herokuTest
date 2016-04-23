a9386ce3e06b1940c64cc1936135463404e9a0e7
samhenrichs <eurostepedits@gmail.com>
2016-04-23 09:41:58 -0500

    Added Devise pages, and also the Amazon tab. Changed the name from herokutest to rewardstest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423115826) do
+ActiveRecord::Schema.define(version: 20160423143911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160423115826) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "amazon"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
